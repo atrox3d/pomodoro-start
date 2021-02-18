f749690a7e4f520f256f21b049433a861a4dfa46
atrox3d <atrox3d@gmail.com>
2021-02-18 09:13:04 +0100

    fix pomodoro offset

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ photo = tkinter.PhotoImage(file="tomato.png")
 #   create canvas
 canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 #   add image
-canvas.create_image(103, 112, image=photo)
-canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.create_image(100, 112, image=photo)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.pack()
 
 window.mainloop()
